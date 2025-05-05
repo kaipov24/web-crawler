d284f0426a45ea3fee39f585f5925ddc0a586b07
kaipov24 <kaipovich24@gmail.com>
2025-05-05 15:39:04 +0600

    Get url

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module crawler
 
 go 1.24.0
+
+require golang.org/x/net v0.39.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-
+	fmt.Println(getURLsFromHTML(`
+<html>
+	<body>
+		<a href="/path/one">
+			<span>Boot.dev</span>
+		</a>
+		<a href="https://other.com/path/one">
+			<span>Boot.dev</span>
+		</a>
+	</body>
+</html>
+`, "https://blog.boot.dev"))
 }
