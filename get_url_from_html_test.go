5d1910bfb66fd6aaa9392134569fce3978711803
kaipov24 <kaipovich24@gmail.com>
2025-05-05 15:02:12 +0600

    Prepare test

---
+++ get_url_from_html_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
+
+func TestGetURLsFromHTML(t *testing.T) {
+	tests := []struct {
+		name          string
+		inputURL      string
+		inputBody     string
+		expected      []string
+		errorContains string
+	}{
+		{
+			name:     "absolute and relative URLs",
+			inputURL: "https://blog.boot.dev",
+			inputBody: `
+		<html>
+			<body>
+				<a href="/path/one">
+					<span>Boot.dev</span>
+				</a>
+				<a href="https://other.com/path/one">
+					<span>Boot.dev</span>
+				</a>
+			</body>
+		</html>
+		`,
+			expected: []string{"https://blog.boot.dev/path/one", "https://other.com/path/one"},
+		},
+	}
+
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := getURLsFromHTML(tc.inputURL, tc.inputBody)
+			if err != nil && !strings.Contains(err.Error(), tc.errorContains) {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			} else if err != nil && tc.errorContains == "" {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			} else if err == nil && tc.errorContains != "" {
+				t.Errorf("Test %v - '%s' FAIL: expected error containing '%v', got none.", i, tc.name, tc.errorContains)
+				return
+			}
+
+			if reflect.DeepEqual(actual, tc.expected) {
+				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
+			}
+
+		})
+	}
+}
